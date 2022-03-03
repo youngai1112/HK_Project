c256324b8e3d5f9305a54910e0e69877ea1d1be8
Gayoung Lee <easy_ant@naver.com>
2022-03-03 14:28:45 +0900

    설명 저장

--- 1_데이터 수집/decorator.py
+++ 1_데이터 수집/decorator.py
@@ -1,9 +1,18 @@
-def younghwan():
-    print("younghwan")
+# 위에서 아래로
 
+var = 1
 
+temp = var + 1
 
+def younghwan():        # 변수에 코드 호출을 실행할 수 있는 값을 담아둠
+    print("younghwan")  
 
 
 def minsoo():
     print("minsoo")
+
+pass
+
+
+
+younghwan()
