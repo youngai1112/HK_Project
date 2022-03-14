ab3dead48dbc2f2a3041bcf6d66253e526e266fd
Gayoung Lee <easy_ant@naver.com>
2022-03-14 21:15:16 +0900

    reload

--- 1_데이터 수집/2_scrapy/tutorial/tutorial/middlewares.py
+++ 1_데이터 수집/2_scrapy/tutorial/tutorial/middlewares.py
@@ -1,3 +1,10 @@
+import os
+from scrapy.http import HtmlResponse
+from scrapy.utils.python import to_bytes
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions as Options
+from selenium.webdriver.common.keys import Keys
+from time import sleep
 # Define here the models for your spider middleware
 #
 # See documentation in:
@@ -64,9 +71,11 @@ class TutorialDownloaderMiddleware:
     @classmethod
     def from_crawler(cls, crawler):
         # This method is used by Scrapy to create your spiders.
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
+        print("this is ScrapyWithSeleniumSpiderMiddleware from_crawler +++++++++++++++++++++++")
+        middleware = cls()
+        crawler.signals.connect(middleware.spider_opened, signals.spider_opened)
+        crawler.signals.connect(middleware.spider_closed, signals.spider_closed)
+        return middleware
 
     def process_request(self, request, spider):
         # Called for each request that goes through the downloader
@@ -78,7 +87,7 @@ class TutorialDownloaderMiddleware:
         # - or return a Request object
         # - or raise IgnoreRequest: process_exception() methods of
         #   installed downloader middleware will be called
-        return None
+        # return None
 
     def process_response(self, request, response, spider):
         # Called with the response returned from the downloader.
@@ -100,4 +109,19 @@ class TutorialDownloaderMiddleware:
         pass
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        dirPath = os.path.dirname(os.path.realpath(__file__))
+        CHROMEDRIVER_PATH = os.path.join(dirPath, r'..\webdriver\chromedriver_win32_99\chromedriver.exe')
+        WINDOW_SIZE = "1920,1080"
+        serviceObject = webdriver.chrome.service.Service(CHROMEDRIVER_PATH)
+        
+        chrome_options = Options()
+        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument(f"--window-size={WINDOW_SIZE}")
+
+        driver = webdriver.Chrome(service=serviceObject, chrome_options=chrome_options)
+        self.driver = driver
+
+    def spider_closed(self, spider):
+        self.driver.close()
