881c075b3ad59b241a3c89fb146f433673d83c3a
Gayoung Lee <easy_ant@naver.com>
2022-03-14 19:55:17 +0900

    items.py에 수집할 데이터 정의

--- 1_데이터 수집/2_scrapy/tutorial/tutorial/items.py
+++ 1_데이터 수집/2_scrapy/tutorial/tutorial/items.py
@@ -4,12 +4,13 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 # data를 class 형태로 만들 수 있다. 
-
+#   제일 먼저 items.py를 정의해야 한다. (가지고 올 데이터를 정의)
+# 연습 : title, 제목, 설명 -> 수집해야 할 데이터
 
 import scrapy
 
 
-class TutorialItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class TutorialItem(scrapy.Item):    # Item을 만들 때, scrapy.Item을 상속받아야 한다.
+    title = scrapy.Field()          # Item을 만들 때, scrapy.Field()를 사용해야 한다. 
+    link = scrapy.Field()
+    desc = scrapy.Field()
