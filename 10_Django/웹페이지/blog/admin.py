ef5b443733d1a61b3c2337ae7a7184cf7b1c441e
Gayoung Lee <easy_ant@naver.com>
2022-04-19 23:04:43 +0900

    Django

---
+++ 10_Django/웹페이지/blog/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from markdownx.admin import MarkdownxModelAdmin
+from .models import Post, Category, Tag, Comment
+
+# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name', )}
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Post, MarkdownxModelAdmin)
+admin.site.register(Comment)
